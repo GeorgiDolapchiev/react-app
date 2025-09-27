a51232f946c02b43c4d6bb1fe89c7edec3ee56d2
Georgi <georgi@Georgis-MacBook-Air.local>
2025-09-27 10:51:45 +0300

    webhook test 4.1

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           >
           </a>
           <h3>
-            CloudAcademy ❤ DevOps 2024 - version 4.0
+            CloudAcademy ❤ DevOps 2024 - version 4.1
           </h3>
         </header>
       </div>
